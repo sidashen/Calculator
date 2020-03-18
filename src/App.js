9fd2de621bb01592903ab94e6868f5a164dfac1b
sidashen <shashasi8023@163.com>
2020-03-18 23:18:23 +0800

    [shasha] feat: calculation functions of minus, multi and divide

--- src/App.js
+++ src/App.js
@@ -19,47 +19,75 @@ class App extends Component {
     this.setState({ [name]: target.value })
   }
 
-  handleCompute = (e) => {
-    let resultNum = this.computeResult();
-    if (e.target.name === '+') {
-      this.setState({
-        result: resultNum
-      })
+  parseCompute(e) {
+    const { result, operator } = this.state;
+    const compute = e.target.name;
+    const newResult = this.computeResult();
+    if (compute === '=') {
+      return {
+        operator: compute,
+        result: newResult
+      };
+    } else if (compute === 'AC') {
+      return {
+        operator: compute,
+        result: 0
+      };
+    } else {
+      return {
+        operator: compute,
+        result: 0
+      }
     }
   }
 
   computeResult() {
-    let { firstNum, lastNum } = this.state;
+    let { operator, firstNum, lastNum } = this.state;
     let computeResult;
-    computeResult = `${parseFloat(firstNum) + parseFloat(lastNum)}`;
-    // if (compute === '-') {
-    //   computeResult = `${Number(temp) - Number(result)}`;
-    // }
-    // if (compute === '*') {
-    //   computeResult = `${Number(temp) * Number(result)}`;
-    // }
-    // if (compute === '/') {
-    //   computeResult = `${Number(temp) / Number(result)}`;
-    // }
-    // if (computeResult.length > 9) {
-    //   return Number(computeResult).toExponential(3);
-    // } else {
+
+    if (operator === '+') {
+      computeResult = `${parseFloat(firstNum) + parseFloat(lastNum)}`;
+    } else if (operator === '-') {
+      computeResult = `${parseFloat(firstNum) - parseFloat(lastNum)}`;
+    } else if (operator === '/') {
+      computeResult = `${parseFloat(firstNum) / parseFloat(lastNum)}`;
+    } else if (operator === '*') {
+      computeResult = `${parseFloat(firstNum) * parseFloat(lastNum)}`;
+    }
     return computeResult;
+  }
+
+  handleCompute = (e) => {
+    this.setState(this.parseCompute(e));
+  }
+
+  clear = (e) => {
+    const target = e.target;
+    const name = target.name;
+    this.setState({ [name]: '' })
+  }
+
+  clearAll = () => {
+    this.setState({
+      result: 0,
+      firstNum: 0,
+      lastNum: 0
+    });
   }
 
   render() {
     return (
       <div>
-        <input value={this.state.result} name="result"></input>
-        <input value={this.state.firstNum} onChange={this.handleChange} name="firstNum"></input>
-        <input value={this.state.lastNum} onChange={this.handleChange} name="lastNum"></input>
+        <input value={this.state.result} name="result" readOnly></input>
+        <input value={this.state.firstNum} onChange={this.handleChange} name="firstNum" onClick={this.clear}></input>
+        <input value={this.state.lastNum} onChange={this.handleChange} name="lastNum" onClick={this.clear}></input>
         <div>
-          <button name="+" onClick={this.handleCompute}>+</button>
-          <button>-</button>
-          <button>/</button>
-          <button>*</button>
-          <button>AC</button>
-          <button>=</button>
+          <button name="+" onClick={(e) => this.handleCompute(e)}>+</button>
+          <button name="-" onClick={(e) => this.handleCompute(e)}>-</button>
+          <button name="/" onClick={(e) => this.handleCompute(e)}>/</button>
+          <button name="*" onClick={(e) => this.handleCompute(e)}>*</button>
+          <button name="AC" onClick={() => this.clearAll()}>AC</button>
+          <button name="=" onClick={(e) => this.handleCompute(e)}>=</button>
         </div>
       </div>
     );
