10ca4e9214225eb3f809b011ac698641a1c40cc1
sidashen <shashasi8023@163.com>
2020-03-18 18:33:40 +0800

    [shasha] feat: add function

--- src/App.js
+++ src/App.js
@@ -10,20 +10,57 @@ class App extends Component {
       firstNum: 0,
       lastNum: 0
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange = (e) => {
-    this.setState({
-      result: e.target.value
-    })
+  handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    this.setState({ [name]: target.value })
+  }
+
+  handleCompute = (e) => {
+    let resultNum = this.computeResult();
+    if (e.target.name === '+') {
+      this.setState({
+        result: resultNum
+      })
+    }
+  }
+
+  computeResult() {
+    let { firstNum, lastNum } = this.state;
+    let computeResult;
+    computeResult = `${parseFloat(firstNum) + parseFloat(lastNum)}`;
+    // if (compute === '-') {
+    //   computeResult = `${Number(temp) - Number(result)}`;
+    // }
+    // if (compute === '*') {
+    //   computeResult = `${Number(temp) * Number(result)}`;
+    // }
+    // if (compute === '/') {
+    //   computeResult = `${Number(temp) / Number(result)}`;
+    // }
+    // if (computeResult.length > 9) {
+    //   return Number(computeResult).toExponential(3);
+    // } else {
+    return computeResult;
   }
 
   render() {
     return (
       <div>
-        <input value={this.state.result} onChange={this.handleChange}></input>
-        <input value={this.state.firstNum} onChange={this.handleChange}></input>
-        <input value={this.state.lastNum} onChange={this.handleChange}></input>
+        <input value={this.state.result} name="result"></input>
+        <input value={this.state.firstNum} onChange={this.handleChange} name="firstNum"></input>
+        <input value={this.state.lastNum} onChange={this.handleChange} name="lastNum"></input>
+        <div>
+          <button name="+" onClick={this.handleCompute}>+</button>
+          <button>-</button>
+          <button>/</button>
+          <button>*</button>
+          <button>AC</button>
+          <button>=</button>
+        </div>
       </div>
     );
   }
